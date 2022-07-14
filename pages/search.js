69c16b927e892b363d523de134e5986e20b29c7e
varsha-316 <varsha160530@gmail.com>
2022-07-14 22:33:42 +0530

    second commit

--- pages/search.js
+++ pages/search.js
@@ -9,7 +9,7 @@ import { baseUrl, fetchApi } from '../utils/fetchApi';
 import SearchFilters from '../components/SearchFilters';
 import noresult from '../assets/images/noresult.svg';
 
-const search = ({properties}) => {
+const Search = ({properties}) => {
     const [searchFilters, setSearchFilters] = useState(false);
     const router = useRouter();
 
@@ -47,7 +47,7 @@ const search = ({properties}) => {
     );
 };
 
-export default search;
+export default Search;
 
 export async function getServerSideProps( { query } ) {
     const purpose = query.purpose || 'for-rent';
